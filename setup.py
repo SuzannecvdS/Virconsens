81cf0ea09391ef85bfe8de574570593a28dc5908
dnieuw <davidnieuwenhuijse@gmail.com>
2025-12-04 10:56:00 +0100

    Update version number to 1.2 in setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 from virconsens import __version__
 
 setup(name='virconsens',
-    version=__version__,
+    version='1.2',
     packages=find_packages(),
     entry_points={
         'console_scripts': [
